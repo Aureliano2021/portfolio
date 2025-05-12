eabd730e1068220218db6a01d9903c5fe608adbd
Aureliano2021 <aureliano.oliveira2011@gmail.com>
2025-05-12 17:32:39 -0300

    Adicionando fotos dos projetos e estilizando thumbs

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -8,14 +8,14 @@ import "swiper/css/pagination";
 export const Portfolio = () => {
 
     const projetosList = [
-        { id: 1, title: "Quiz", shortDescription: "Projeto de um Quiz, exibindo resultado final usando ReactJS com NextJS.", link: "https://quiz-liard-zeta.vercel.app/", codigo: "https://github.com/Aureliano2021/quiz" },
-        {id: 2, title: "Galeria React", shortDescription: "Projeto de uma Galeria de fotos feito em React", link: "https://galeria-react-sable.vercel.app/", codigo: "https://github.com/Aureliano2021/Galeria-React"},
-        { id: 3, title: "Currículo", shortDescription: "Projeto de currículo profissional usando HTML5, CSS3, JavaScript e TypeScript.", link: "https://aureliano2021.github.io/", codigo: "https://github.com/Aureliano2021/aureliano2021.github.io" },
-        {id: 4, title: "Pizza JavaScript", shortDescription: "Projeto de vendas de pizza usando JavaScript", link: "https://pizza-javascript.vercel.app/", codigo: "https://github.com/Aureliano2021/Pizza-JavaScript"},
-        { id: 5, title: "Bateria JavaScript", shortDescription: "Projeto de Bateria emitindo soms, código feito com JavaScript.", link: "https://bateriajavascript.vercel.app/", codigo: "https://github.com/Aureliano2021/Bateria" },
-        {id: 6, title: "Clima API", shortDescription: "Projeto para dizer o clima de uma cidade usando api OpenWeather", link: "https://clima-api-zeta.vercel.app/", codigo:"https://github.com/Aureliano2021/ClimaAPI"},
-        {id: 7, title: "Jogo da Velha", shortDescription: "Projeto de Jogo da Velha com interface usando JavaScript", link: "https://jogo-da-velha-five-mocha.vercel.app/", codigo: "https://github.com/Aureliano2021/Jogo-da-Velha"},
-        {id: 8, title: "Relógio Analógico/Digital", shortDescription: "Projeto de um relógio analógico e digital usando JavaScript, HTML e CSS",link: "https://clock-ad.vercel.app/", codigo: "https://github.com/Aureliano2021/ClockAD"}
+        { id: 1, title: "Quiz", shortDescription: "Projeto de um Quiz, exibindo resultado final usando ReactJS com NextJS.", link: "https://quiz-liard-zeta.vercel.app/", codigo: "https://github.com/Aureliano2021/quiz", image: "/media/Quiz.jpg" },
+        {id: 2, title: "Galeria React", shortDescription: "Projeto de uma Galeria de fotos feito em React", link: "https://galeria-react-sable.vercel.app/", codigo: "https://github.com/Aureliano2021/Galeria-React", image: "/media/Gallery.jpg"},
+        { id: 3, title: "Currículo", shortDescription: "Projeto de currículo profissional usando HTML5, CSS3, JavaScript e TypeScript.", link: "https://aureliano2021.github.io/", codigo: "https://github.com/Aureliano2021/aureliano2021.github.io", image: "/media/curriculo.png"},
+        {id: 4, title: "Pizza JavaScript", shortDescription: "Projeto de vendas de pizza usando JavaScript", link: "https://pizza-javascript.vercel.app/", codigo: "https://github.com/Aureliano2021/Pizza-JavaScript" , image: "/media/Pizza.jpg"},
+        { id: 5, title: "Bateria JavaScript", shortDescription: "Projeto de Bateria emitindo soms, código feito com JavaScript.", link: "https://bateriajavascript.vercel.app/", codigo: "https://github.com/Aureliano2021/Bateria", image: "/media/Drums.jpg"},
+        {id: 6, title: "Clima API", shortDescription: "Projeto para dizer o clima de uma cidade usando api OpenWeather", link: "https://clima-api-zeta.vercel.app/", codigo:"https://github.com/Aureliano2021/ClimaAPI", image: "/media/Weather.jpg"},
+        {id: 7, title: "Jogo da Velha", shortDescription: "Projeto de Jogo da Velha com interface usando JavaScript", link: "https://jogo-da-velha-five-mocha.vercel.app/", codigo: "https://github.com/Aureliano2021/Jogo-da-Velha", image: "/media/OldGame.jpg"},
+        {id: 8, title: "Relógio Analógico/Digital", shortDescription: "Projeto de um relógio analógico e digital usando JavaScript, HTML e CSS",link: "https://clock-ad.vercel.app/", codigo: "https://github.com/Aureliano2021/ClockAD", image: "/media/Clock.jpg"}
         
     ]
 
@@ -41,7 +41,7 @@ export const Portfolio = () => {
                     {projetosList.map((projeto) => (
                         <SwiperSlide key={projeto.id}>
                             <div className="border border-gray-700 justify-center items-center flex flex-col rounded-lg">
-                                <a href={projeto.link} target="_blank"><div className="w-30 h-30 bg-cover bg-top my-4 rounded-lg" style={{ backgroundImage: "url('/media/Logo.jpeg')" }}></div></a>
+                                <a href={projeto.link} target="_blank"><div className="w-48 h-30 bg-cover bg-top my-4 rounded-lg hover:brightness-50 transition duration-300" style={{ backgroundImage: `url(${projeto.image})` }}></div></a>
                                 <div className="bg-gray-700 flex flex-col items-start m-2 p-2 rounded-md">
                                     <h2 className="text-gray-300 text-lg font-semibold">{projeto.title}</h2>
                                     <p className="text-gray-400 text-sm">{projeto.shortDescription}</p>
